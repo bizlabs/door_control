cb93cd6057d1daf2cb7b0bfef1761c7cd36528a0
Doug <doug@bizlabs.org>
2024-04-16 22:13:13 -0500

    fix import users to not save back to controller

--- door_control/door_control/doctype/doorset/doorset.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Doug Mattingly and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class doorset(Document):
-	pass
